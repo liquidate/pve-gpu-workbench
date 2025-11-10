a5010f97af1d9d7a428fdf4a9cff9a31babb4316
liquidate <liquidate@users.noreply.github.com>
2025-11-10 12:57:01 -0800

    Remove .old-docker-scripts folder - all functionality replaced

--- .old-docker-scripts/999 - upgrade-proxmox.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# SCRIPT_DESC: Upgrade Proxmox to latest version
-# SCRIPT_DETECT: [ "$(apt list --upgradable 2>/dev/null | grep -c upgradable)" -eq 0 ]
-
-echo ">>> Upgrading Proxmox VE to the latest version"
-pveupgrade
-echo ">>> Proxmox VE upgrade completed."
